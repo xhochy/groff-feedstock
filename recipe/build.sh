626ecd2f39877a410d346eca8ffb68744e869e77
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-16 02:08:43 -0800

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -54,7 +54,7 @@ find $SRC_DIR -type f | \
         -pe "s,perl -w,perl,;" \
         -pe "s,$PREFIX/bin/perl,/usr/bin/env perl,;"
 
-make --trace -j${CPU_COUNT} install "${make_args}"
+make --trace -j${CPU_COUNT} install ${make_args}
 # if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
 # make check
 # fi
